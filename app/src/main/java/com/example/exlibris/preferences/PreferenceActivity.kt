dbceb41da3c4483c3399c81996eb0f93d54c09e0
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-11-08 20:12:45 -0300

    Se crea Preference Activity (sin funcionalidad)

---
+++ app/src/main/java/com/example/exlibris/preferences/PreferenceActivity.kt
@@ -0,0 +1,12 @@
+package com.example.exlibris.preferences
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.exlibris.R
+
+class PreferenceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preference)
+    }
+}
