d450f3f645160ffc2978c2c60f1da0b36a1985a7
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-11-08 20:06:56 -0300

    Se agrega SplashScreen

---
+++ app/src/main/java/com/example/exlibris/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.example.exlibris
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed(
+            {
+                launchMainActivity()
+                finish()
+            }, 2500
+        )
+    }
+
+    private fun launchMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
