1b8e7667c38a17e0ef27decb58f6105c5614323d
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-11-08 21:03:43 -0300

    Se comienza a implementar SQLite

---
+++ app/src/main/java/com/example/exlibris/db/BookDao.kt
@@ -0,0 +1,25 @@
+package com.example.exlibris.db
+
+import android.content.Context
+import com.example.exlibris.data.Book
+import com.j256.ormlite.android.apptools.OpenHelperManager
+import com.j256.ormlite.dao.Dao
+
+class BookDao(private val context: Context) {
+    private var dao: Dao<Book, Int>
+
+    init {
+        val helper = OpenHelperManager.getHelper(context, DBHelper::class.java)
+        dao = helper.getDao(Book::class.java)
+    }
+
+    fun addBook(book: Book) = dao.create(book)
+
+    fun deleteBook(book: Book) = dao.delete(book)
+
+    fun updateBook(book: Book) = dao.update(book)
+
+    fun getBook() = dao.queryForAll()
+
+    fun getBooks(bookId: Int) = dao.queryForId(bookId)
+}
