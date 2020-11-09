f39d01698b69ad97284fcdbcc5b1cf75a3765f73
chrisXIII <cristian_a_rojas@hotmail.com>
2020-11-08 21:14:40 -0300

    Creacion de la toolbar y el menu

--- app/src/main/java/com/example/exlibris/MainActivity.kt
+++ app/src/main/java/com/example/exlibris/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.exlibris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.itemSettings) {
+            //realizar action
+        }
+
+        return super.onOptionsItemSelected(item)
+
+
+    }
+/*
+    private fun launchSettings() {
+        startActivity(
+                Intent(this, PreferenceActivity::class.java)
+        )
+    }*/
 }
