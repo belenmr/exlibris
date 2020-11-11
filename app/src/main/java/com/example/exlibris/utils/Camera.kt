9453cbb082c676ab80f73faedd4b5b2652106d62
Belen Medina Roldan <bmedinaroldan@gmail.com>
2020-11-11 19:24:52 -0300

    Se corrige utilidad para tomar foto

--- app/src/main/java/com/example/exlibris/utils/Camera.kt
+++ app/src/main/java/com/example/exlibris/utils/Camera.kt
@@ -1,68 +1,127 @@
 package com.example.exlibris.utils
 
 import android.app.Activity
-import android.app.Activity.RESULT_OK
 import android.content.Intent
-import android.graphics.Bitmap
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.ImageView
-import androidx.core.app.ActivityCompat.startActivityForResult
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.FileProvider
 import java.io.File
-import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
 class Camera(
         private var activity: Activity,
         private var imageView: ImageView) {
-    private val REQUEST_TAKE_PHOTO = 1
-    lateinit var currentPhotoPath: String
 
-    fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(activity.packageManager)?.also {
+        private val REQUEST_TAKE_PHOTO = 1
+        private val AUTHORITY = "com.example.exlibris"
+        private val permissionCamera = android.Manifest.permission.CAMERA
+        private val permissionWriteStorage = android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        private val permissionReadStorage = android.Manifest.permission.READ_EXTERNAL_STORAGE
 
-                val photoFile: File? = try {
-                    createImageFile()
-                } catch (ex: IOException){
-                    Log.e("ERROR_CAMARA", "Error al tomar foto")
-                    null
-                }
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                            activity.applicationContext,
-                            "com.example.exlibris.fileprovider",
-                            it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    activity.startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+        var urlFotoActual = " "
+
+        fun takePhoto(){
+            requestPermission()
+        }
+
+
+        private fun requestPermission() {
+            ActivityCompat.shouldShowRequestPermissionRationale(activity, permissionCamera)
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                activity.requestPermissions(arrayOf(permissionCamera,permissionWriteStorage,permissionReadStorage), REQUEST_TAKE_PHOTO)
+            }
+        }
+
+
+        fun requestPermissionsResult(
+                requestCode: Int,
+                permissions: Array<out String>,
+                grantResults: IntArray
+        ) {
+
+            when(requestCode){
+                REQUEST_TAKE_PHOTO -> {
+                    if(grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[1] == PackageManager.PERMISSION_GRANTED &&
+                            grantResults[2] == PackageManager.PERMISSION_GRANTED){
+                        //tenemos permiso
+                        dispatchTakePictureIntent()
+                    } else {
+                        //no tenemos permiso
+                        Toast.makeText(activity.applicationContext, "No diste permiso para acceder a la camara y almacenamiento", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
-    }
 
-    fun activityResult(requestCode: Int, resultCode: Int, data: Intent){
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            val imageBitmap = data.extras?.get("data") as Bitmap
+
+        private fun dispatchTakePictureIntent(){
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            if(intent.resolveActivity(activity.packageManager) != null){
+
+                var archivoFoto: File? = null
+                archivoFoto = createImageFile()
+
+                if(archivoFoto != null){
+                    var urlFoto = FileProvider.getUriForFile(activity.applicationContext, AUTHORITY, archivoFoto)
+
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, urlFoto)
+                    activity.startActivityForResult(intent, REQUEST_TAKE_PHOTO)
+                }
+            }
+        }
+
+
+        fun activityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+            when(requestCode){
+                REQUEST_TAKE_PHOTO -> {
+                    if(resultCode == AppCompatActivity.RESULT_OK){
+                        //obtener imagen
+                        Log.d("ACTIVITY_RESULT", "obtener imagen")
+                        /*val extras = data?.extras //tiene la info de toda la camara
+                        val imageBitmap = extras?.get("data") as Bitmap
+                        */
+                        showBitmap(urlFotoActual)
+
+                    } else {
+                        //se cancelo la captura
+                    }
+                }
+            }
+        }
+
+        private fun showBitmap(url:String){
+            val uri = Uri.parse(url)
+            val stream = activity.contentResolver.openInputStream(uri)
+            val imageBitmap = BitmapFactory.decodeStream(stream)
             imageView.setImageBitmap(imageBitmap)
         }
-    }
 
-    //@Throws(IOException::class)
-    private fun createImageFile(): File {
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val imageFIle =        return File.createTempFile(
-                "EXLIBRIS_${timeStamp}_",
-                ".jpg",
-                storageDir /* directory */
-        ).apply {
-            // Save a file: path for use with ACTION_VIEW intents
-            currentPhotoPath = absolutePath
+
+        private fun createImageFile(): File{
+            val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+            val nombreArchivoImagen = "Exlibris_" + timeStamp + "_"
+
+            val directorio = activity.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+            val imagen = File.createTempFile(nombreArchivoImagen,".jpg",directorio)
+
+            urlFotoActual = "file://" + imagen.absolutePath //me regresa toda la url de la imagen
+
+            return imagen
         }
-    }
+
+
 }
