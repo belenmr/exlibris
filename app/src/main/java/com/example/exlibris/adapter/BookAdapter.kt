062dd4804ea5280c34e87c7a28b1db92fff900aa
Belen Medina Roldan <bmedinaroldan@gmail.com>
2021-02-27 04:17:35 -0300

    Se agrega detalle de checkbox read a vista principal

--- app/src/main/java/com/example/exlibris/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/exlibris/adapter/BookAdapter.kt
@@ -13,6 +13,8 @@ import com.example.exlibris.data.Book
 import kotlinx.android.synthetic.main.item_books.view.*
 
 const val COMPLETED_BOOK = "Book"
+const val READ_BOOK = "Leido"
+const val NOT_READ_BOOK = "Pendiente"
 
 class BookAdapter(val listener: MainActivity):RecyclerView.Adapter<BookAdapter.BookHolder>(){
 
@@ -34,6 +36,7 @@ class BookAdapter(val listener: MainActivity):RecyclerView.Adapter<BookAdapter.B
         fun render (books:Book){
             view.tv_Title.text = books.name
             view.tv_Author.text = books.author
+            view.tv_read.text = showReadBook(books)
             view.ivBook.setImageBitmap(BitmapFactory.decodeFile(books.resImage))
             view.setOnClickListener{
                 val intent = Intent(view.context,BookActivity::class.java)
@@ -41,12 +44,24 @@ class BookAdapter(val listener: MainActivity):RecyclerView.Adapter<BookAdapter.B
                 view.getContext().startActivity(intent)
             }
         }
+
+        private fun showReadBook(book: Book): String {
+            var read = book.read
+            var textRead = NOT_READ_BOOK
+
+            if(read){
+                textRead = READ_BOOK
+            }
+
+            return textRead
+        }
     }
 
     fun updateBooks(books: List<Book>) {
         this.books = books
         notifyDataSetChanged()
     }
+
 
     interface BookListener {
 
